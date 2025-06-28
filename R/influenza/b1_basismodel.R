4fb6d22266db60cfe673d8be0064b6391a7f69ed
Katarina Matthes <katarina.matthes@iem.uzh.ch>
2025-06-28 16:57:13 +0200

    minor changes

--- R/influenza/b1_basismodel.R
+++ R/influenza/b1_basismodel.R
@@ -8,12 +8,12 @@ col_c  <- c("#7030A0","#1EA7C4","#F6A1CAFF","#EB7746FF","#B9CA5DFF", "#1A1F2BFF"
 col_sun <- c("#F6A1CAFF", "#1A1F2BFF","#EB7746FF", "#B9CA5DFF","#FBDB82FF", "#9F272EFF", "#D36B8DFF", "#EC9E58FF", "#24719BFF", "#6A1639FF")
 
 lwd_size <- 3
-size_plot <- 22
-size_strip <- 22
-axis_text_size <- 22
-axis_title_size <- 22
-plot_title_size <- 22
-legend_text_size <- 25
+size_plot <- 30
+size_strip <- 30
+axis_text_size <- 30
+axis_title_size <- 30
+plot_title_size <- 30
+legend_text_size <- 30
 
 # Define SEIRD Model
 # Parameters mild
@@ -28,16 +28,22 @@ CFR <- 0.007      # Case fatality rate
 p_hosp <- 0.03 # Proportion of infections hospitalized
 p_die_hosp <- 0.25    # Proportion of hospitalized who die
 
-sigma <- 1 / 2        # Incubation rate (2 days)
-gamma <- 1 / 5        # Recovery rate (5 days)
+# for both
+incub_period <- 2
+infection_period <- 5
 Hr <-  10 # times in hospital
 
+# calculate from given assumptions
+sigma <- 1 / incub_period        # Incubation rate (2 days)
+gamma <- 1 / infection_period        # Recovery rate (5 days)
 mu <- p_die_hosp / Hr      # Death rate in hospital
 rho <- (1 - p_die_hosp) / Hr  # Recovery rate in hospital
+beta <- R0 * gamma   # Transmission rate
+
 
 # Population and transmission rate
 N <- 100000
-beta <- R0 * gamma   # Transmission rate
+
 
 
 # Initial state
@@ -124,6 +130,8 @@ out_df <- as.data.frame(out) %>%
   gather(., comp, prop,S:D) %>%
   filter(comp %in% c("H", "D"))
 
+col_c  <- c("#EB7746FF", "#1A1F2BFF")
+
 ggplot(out_df, aes(x = time)) +
   geom_line(aes(y = prop, color = comp), lwd=lwd_size) +
   labs(title = "Influenza - Mild Scenario",
@@ -136,16 +144,15 @@ ggplot(out_df, aes(x = time)) +
   # scale_y_continuous(breaks = seq(0, 100000, by = 10000))+
   theme_bw()+
   theme(
-    strip.text = element_text(size=size_plot),
-    axis.text = element_text(size=axis_text_size),
-    axis.title  = element_text(size=axis_title_size),
+    axis.text = element_text(size=axis_text_size+10),
+    axis.title  = element_text(size=axis_title_size+10),
     legend.position = "bottom",
-    legend.text=element_text(size=legend_text_size),
-    plot.title = element_text(size=plot_title_size),
+    legend.text=element_text(size=legend_text_size+10),
+    plot.title = element_text(size=plot_title_size+10),
     panel.grid.minor.x = element_blank(),
     panel.grid.minor.y = element_blank())
 
-ggsave("figures/influenza/mild_zoom.png",h=12,w=15)
+ggsave("figures/influenza/mild_zoom.png",h=10,w=10)
 
 
 ggplot(out_df, aes(x = time)) +
@@ -159,15 +166,15 @@ ggplot(out_df, aes(x = time)) +
                      values = col_c) +
   # scale_y_continuous(breaks = seq(0, 100000, by = 10000))+
   theme_bw()+
-  theme(
-    axis.text = element_text(size=axis_text_size),
-    axis.title  = element_text(size=axis_title_size),
-    legend.position = "bottom",
-    legend.text=element_text(size=legend_text_size),
-    plot.title = element_text(size=plot_title_size),
-    panel.grid.minor.x = element_blank(),
-    panel.grid.minor.y = element_blank())
+    theme(
+      axis.text = element_text(size=axis_text_size+10),
+      axis.title  = element_text(size=axis_title_size+10),
+      legend.position = "bottom",
+      legend.text=element_text(size=legend_text_size+10),
+      plot.title = element_text(size=plot_title_size+10),
+      panel.grid.minor.x = element_blank(),
+      panel.grid.minor.y = element_blank())
 
-ggsave("figures/influenza/severe_zoom.png",h=12,w=15)
+ggsave("figures/influenza/severe_zoom.png",h=10,w=10)
 
 
